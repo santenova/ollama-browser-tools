022d3d1267b1c45746fe4638e8b414227b007014
santenova <tokenfreeai@gmail.com>
2026-06-27 21:12:52 +0200

    npm install; npm run postinstall; npm  run ingest; npm run start;

--- data/import.sh
+++ data/import.sh
@@ -1,6 +1,5 @@
 
-for i in $(ls *json); do
-
+for i in Persona.json Template.json; do
 idx=$(echo "sample-prompt-$i" | tr [A-Z] [a-z] | tr -d "_" | sed "s/.json//"); 
-python3.9  ./import.py --url http://127.0.0.1:9200 --file ./$i --index $idx;
+echo ./venv/bin/python3  ./import.py --url http://127.0.0.1:9200 --file ./$i --index $idx;
 done
